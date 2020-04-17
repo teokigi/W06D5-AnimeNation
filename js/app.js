6534225f160e5ddff7beb3078102dee416ec4db8
Tony Tang <teokigi@hotmail.com>
2020-04-17 14:37:19 +0100

    opening commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// MVP
+// Create a form in HTML with inputs for relevant data
+// When the form is submitted, access the data from the form in the form’s submit event object
+// Create a list in HTML
+// Append the submitted data to the list
+// Add a “Delete All” <button> which clears all of the list items from the list
+// Note: If you are at all unclear about the process of reading and writing to the DOM, complete the MVP, writing all the code in one function. Understanding the document’s methods is the main learning for this homework. Abstracting code into helper functions is secondary.
+//
+// Extensions
+// Refactor your code, extracting helper functions with single responsibilites to keep each function small and readable
+// Experiment with adding a new form input and experimenting with a new <input> type eg. Add a radio button input
+// Style your application with CSS
+// Add any other extension functionality that you want!
