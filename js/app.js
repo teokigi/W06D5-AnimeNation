836c37cb97731a843d50403faf83db50dcf45f1b
teokigi <teokigi@hotmail.com>
2020-04-18 02:11:16 +0100

    updated mvp

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 // MVP
 // Create a form in HTML with inputs for relevant data - done
 // When the form is submitted, access the data from the form in the form’s submit event object - done
-// Create a list in HTML - to do
-// Append the submitted data to the list - to do
+// Create a list in HTML - done
+// Append the submitted data to the list - done
 // Add a “Delete All” <button> which clears all of the list items from the list - done
 // Note: If you are at all unclear about the process of reading and writing to the DOM, complete the MVP, writing all the code in one function. Understanding the document’s methods is the main learning for this homework. Abstracting code into helper functions is secondary.
 //
