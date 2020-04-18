a8066be8dd1fdbf189195edee881dc8a2316bfb7
teokigi <teokigi@hotmail.com>
2020-04-18 13:34:43 +0100

    added a method to store/clear session data

--- js/app.js
+++ js/app.js
@@ -1,20 +1,29 @@
 // MVP
-// Create a form in HTML with inputs for relevant data - done
-// When the form is submitted, access the data from the form in the form’s submit event object - done
-// Create a list in HTML - done
-// Append the submitted data to the list - done
-// Add a “Delete All” <button> which clears all of the list items from the list - done
-// Note: If you are at all unclear about the process of reading and writing to the DOM, complete the MVP, writing all the code in one function. Understanding the document’s methods is the main learning for this homework. Abstracting code into helper functions is secondary.
+// done - Create a form in HTML with inputs for relevant data.
+// done - When the form is submitted, access the data from the form in the form’s submit event object.
+// done - Create a list in HTML.
+// done - Append the submitted data to the list.
+// done - Add a “Delete All” <button> which clears all of the list items from the list.
+// Note: If you are at all unclear about the process of reading and writing to the DOM,
+//       complete the MVP, writing all the code in one function.
+//       Understanding the document’s methods is the main learning for this homework.
+//       Abstracting code into helper functions is secondary.
 //
-// Extensions
-// Refactor your code, extracting helper functions with single responsibilites to keep each function small and readable
-// Experiment with adding a new form input and experimenting with a new <input> type eg. Add a radio button input
+// Extensions:
+// to do - Refactor code, extracting helper functions with single responsibilites
+//                        to keep each function small and readable.
+// Experiment with adding a new form input and experimenting with a new <input> type eg.
+// Add a radio button input.
 // Style your application with CSS
 // Add any other extension functionality that you want!
 document.addEventListener('DOMContentLoaded', () => {
     //logging to console javascript loaded
     console.log('JavaScript has loaded');
     //function that handles the form submittion
+    if (sessionStorage.getItem("animeVids")){
+    const inheritDivBox = document.getElementById("createdListings");
+    inheritDivBox.innerHTML = sessionStorage.getItem('animeVids');
+    };
     const form = document.querySelector('#form');
     form.addEventListener('submit', formHandler);
 
@@ -27,6 +36,7 @@ const deleteEverything = function(event){
     const  getDivBox = document.querySelector("#createdListings");
 
     getDivBox.innerHTML = '';
+    sessionStorage.clear();
     
 }
 
@@ -63,6 +73,7 @@ const formHandler = function(event){
         newItemBox.appendChild(newUnorderedList);
         
         getDivBox.appendChild(newItemBox);
-
+        
+        sessionStorage.setItem('animeVids', getDivBox.innerHTML);
         event.target.reset();
 }
